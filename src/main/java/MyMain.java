00b6e0fef3773ffd7701e7cc342d9cc9a2dbfc33
AaravSrivastav <22asrivastav@athenian.org>
2020-09-02 02:16:59 +0000

    Added a chatbot method to main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + "! It's nice to meet you!");
 
+        System.out.println("What is your favorite color, " + name + "?");
+        String color = scan.nextLine();
+        System.out.println("I like " + color + " too!");
+
+        System.out.println("What is your favorite movie, " + name + "?");
+        String movie = scan.nextLine();
+        System.out.println(movie + " is pretty good.");
+        
+        System.out.println("What is your favorite food, " + name + "?");
+        String food = scan.nextLine();
+        System.out.println(food + " tastes good.");
+
+        System.out.println("What is your favorite class, " + name + "?");
+        String school = scan.nextLine();
+        System.out.println("Oh yeah, " + school + " is fun.");
 
         scan.close();
     }
